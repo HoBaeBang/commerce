c83f4a24d4149024bd0456bcf466017772c90075
HobaeBang <bhb0407@naver.com>
2025-05-02 23:01:07 +0900

    Refactor : ProductController RED, GREEN case 추가 & 오타 수정

--- cmc/src/main/java/com/aslan/cmc/products/service/ProductsServices.java
+++
@@ -1,9 +0,0 @@
-package com.aslan.cmc.products.service;
-
-import com.aslan.cmc.products.data.SortDirection;
-
-public interface ProductsServices {
-
-    public void getProductsList(String keyword, int start, int length, int min_price, int max_price, SortDirection sort);
-
-}
