9bf125d8fcc9a4576e36e592969b2a0a6be73c13
HobaeBang <bhb0407@naver.com>
2025-05-02 21:30:32 +0900

    Refactor : 약자 -> 풀네임으로 변경

--- cmc/src/main/java/com/aslan/cmc/CommerceApplication.java
+++ cmc/src/main/java/com/aslan/cmc/CommerceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CmcApplication {
+public class CommerceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CmcApplication.class, args);
+        SpringApplication.run(CommerceApplication.class, args);
     }
 
 }
